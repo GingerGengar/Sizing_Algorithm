f363aecc571b496cd5e1c8e2417cda325e241820
GingerGengar <GingerGengar123@gmail.com>
2022-09-09 20:01:21 -0400

    Added a global variable Definition

---
+++ Global_Variables.m
@@ -0,0 +1,9 @@
+%Global variable list
+global L_wi; %This is a wing loading ()
+global rho; %This is the air density (kg/m^3)
+global v_stall %This is the stall speed of our aircraft (m/s)
+global c_Lmax %This is an estimation of how good is our biggest possible lift coefff
+
+
+
+
