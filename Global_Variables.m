8caa6184538290b76723e3907701343ce042cecb
GingerGengar <GingerGengar123@gmail.com>
2022-09-11 22:12:02 -0400

    Added capabilities to compute the battery mass/weight

--- Global_Variables.m
+++ Global_Variables.m
@@ -1,4 +1,4 @@
-%Global variable list
+%%Global variable list
 global L_wi; %This is a wing loading (N/m^2)
 global T_W; %This is the thrust to weight ratio (dimensionless)
 global rho; %This is the air density (kg/m^3)
@@ -9,5 +9,8 @@ global c_D0; %This is the drag coefficient near stall speed
 global A_r; %Aspect ratio of the main wing
 global e; %efficiency of the wing due to deviating from elliptic lift distribution
 global G; %Gradient of ascent = rise of flight/run of flight
-
+global endurance; %This is how long the aircraft should be flying in terms of seconds
+global Motor_Power; %This is how much power the motor consumes (J/s)
+global Batt_Specific_Energy; %This has units of (J/kg)
+global Batt_Mass; %This has units of kg, this is the mass of the battery
 
--- Initializations.m
+++ Initializations.m
@@ -1,7 +1,9 @@
-%Give Definition of Global variable
+%%Give Definition of Global variable
 Global_Variables;
 
-%Variables which are known/guessed (independent)
+%%Variables which are known/guessed (independent)
+
+%Variables Needed to Compute the Wing Loading and Thrust to Weight Ratio
 rho = 1.225; %This is the density of the air
 v_stall = 8; %This is the estimation for stall speed
 c_Lmax = 1.2; %the maximum lift coefficient we can get
@@ -10,7 +12,15 @@ c_D0 = 0.03; %This is the drag coefficient near stall speed
 A_r = 9; %Aspect ratio of the main wing, typical values
 e = 0.8; %efficiency of the wing due to deviating from elliptic lift distribution
 
-%Variables that are dependent on other variables (dependent)
+%Variables that are needed to compute the weight of the aircraft
+
+
+%Variables that are needed to compute the weight of the powerplant system
+endurance = 900; %seconds of flight. This corresponds to 15 minutes of flight
+Motor_Power = ; %This is the power of the motor (W)
+Batt_Specific_Energy = ; %This is Energy/Mass, so its unit has to be (J/kg)
+
+%%Variables that are dependent on other variables (dependent)
 q = 1/2*rho*v_stall^2; %This is the dynamic pressure
 G = tan(deg2rad(climb_angle)); %Gradient of ascent = rise of flight/run of flight
 
