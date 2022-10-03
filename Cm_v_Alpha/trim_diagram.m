33e51b51a4128cc20d0b01c520b3d50097887aab
Dan Qi <qi85@purdue.edu>
2022-10-03 19:37:50 -0400

    trim plot

---
+++ Cm_v_Alpha/trim_diagram.m
@@ -0,0 +1,42 @@
+trim_data = cell(6,1);
+
+trim_data{1} = readmatrix('trim_neg_3.csv');
+trim_data{2} = readmatrix('trim_neg_1_5.csv');
+trim_data{3} = readmatrix('trim_0.csv');
+trim_data{4} = readmatrix('trim1_5.csv');
+trim_data{5} = readmatrix('trim2_5.csv');
+trim_data{6} = readmatrix('trim3.csv');
+trim_list = ["-3^{\circ}","-1.5^{\circ}","0^{\circ}","+1.5^{\circ}","+2.5^{\circ}","+3^{\circ}"];
+picked_trim = 5;
+
+figure(1)
+for i = 1:6
+    aoa = trim_data{i}(:,1);
+    cm = trim_data{i}(:,2);
+    if i == picked_trim
+        plot(aoa,cm)
+        hold on
+        [trim_aoa,ind]=min(abs(cm));
+        plot(aoa(ind),cm(ind),'r*')    
+    else
+        plot(aoa,cm,'--')
+    end
+    hold on
+    
+    % Plot the Text
+    mover = 8;
+    [~,ind]=min(abs(aoa-mover));
+    text(aoa(ind),cm(ind)+0.01,trim_list(i))
+end
+yline(0,'k')
+hold on
+xline(0,'k')
+grid on
+xlim([-5,10])
+xlabel('\alpha (deg)')
+ylabel('C_m')
+title('C_m vs \alpha')
+
+% hleg = legend(trim_list);
+% htitle = get(hleg,'Title');
+% set(htitle,'String','Trim Condition')
